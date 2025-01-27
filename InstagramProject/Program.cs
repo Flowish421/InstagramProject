9e5bb02af9eb8ae32504642d091fd2bc5296fe63
ITHanan <155847905+ITHanan@users.noreply.github.com>
2025-01-27 13:19:22 +0100

    creation of instagram menu

--- InstagramProject/Program.cs
+++ InstagramProject/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using InstagramProject.Models;
+
 Console.WriteLine("Hello, World!");
+DisplayInstagramMenu instagram = new DisplayInstagramMenu();
+instagram.RunSystem();

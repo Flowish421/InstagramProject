b2c0665257a5c26afe44fa231675cc29842f7d89
NiklasRoos91 <niklas_roos@live.se>
2025-01-27 11:54:12 +0100

    Kopplat databas

---
+++ InstagramProject/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InstagramProject.Models;
+
+public partial class Comment
+{
+    public int CommentId { get; set; }
+
+    public string? CommentText { get; set; }
+
+    public DateTime? Timestamp { get; set; }
+
+    public int? PostId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public virtual Post? Post { get; set; }
+
+    public virtual User? User { get; set; }
+}
---
+++ InstagramProject/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InstagramProject.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Email { get; set; }
+
+    public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+    public virtual ICollection<Like> Likes { get; set; } = new List<Like>();
+
+    public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
+
+    public virtual ICollection<Share> Shares { get; set; } = new List<Share>();
+}
